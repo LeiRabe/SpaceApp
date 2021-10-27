46f4423fb03a804f35020f5a70c7b1f54844d861
Noelle15 <noelle.verma152000@gmail.com>
2021-10-27 10:55:00 +0200

    initial commit

---
+++ app/src/main/java/com/example/spaceapp/MainActivity.kt
@@ -0,0 +1,37 @@
+package com.example.spaceapp
+
+import android.os.Bundle
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentActivity
+import androidx.fragment.app.FragmentManager
+import androidx.navigation.Navigation
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import com.example.spaceapp.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+     binding = ActivityMainBinding.inflate(layoutInflater)
+     setContentView(binding.root)
+
+        val navView: BottomNavigationView = binding.navView
+
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment_activity_main)
+        // Passing each menu ID as a set of Ids because each
+        // menu should be considered as top level destinations.
+        val appBarConfiguration = AppBarConfiguration(setOf(
+            R.id.navigation_home, R.id.navigation_iss, R.id.navigation_favorite, R.id.navigation_quiz))
+        setupActionBarWithNavController(navHostFragment!!.findNavController(), appBarConfiguration)
+        navView.setupWithNavController(navHostFragment!!.findNavController())
+    }
+}
