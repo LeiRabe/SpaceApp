0ec767cb40c8b1dc7bd6053f679fa8cb49fcd791
Noelle15 <noelle.verma152000@gmail.com>
2021-11-12 11:56:47 +0100

    actionBar + mockup vues

--- app/src/main/java/com/example/spaceapp/ui/home/HomeFragment.kt
+++ app/src/main/java/com/example/spaceapp/ui/home/HomeFragment.kt
@@ -1,44 +1,71 @@
 package com.example.spaceapp.ui.home
 
+import android.app.ActionBar
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.FrameLayout
+import android.widget.LinearLayout
 import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
+import com.example.spaceapp.MainActivity
 import com.example.spaceapp.R
 import com.example.spaceapp.databinding.FragmentHomeBinding
+import com.google.android.material.bottomsheet.BottomSheetBehavior
+import com.google.android.material.bottomsheet.BottomSheetDialog
+
 
 class HomeFragment : Fragment() {
+    //bottomSheet
+    private lateinit var bottomSheetBehavior: BottomSheetBehavior<FrameLayout>
 
-  private lateinit var homeViewModel: HomeViewModel
-private var _binding: FragmentHomeBinding? = null
-  // This property is only valid between onCreateView and
-  // onDestroyView.
-  private val binding get() = _binding!!
 
-  override fun onCreateView(
-    inflater: LayoutInflater,
-    container: ViewGroup?,
-    savedInstanceState: Bundle?
-  ): View? {
-    homeViewModel =
+    private lateinit var homeViewModel: HomeViewModel
+    private var _binding: FragmentHomeBinding? = null
+
+    // This property is only valid between onCreateView and
+    // onDestroyView.
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        homeViewModel =
             ViewModelProvider(this).get(HomeViewModel::class.java)
 
-    _binding = FragmentHomeBinding.inflate(inflater, container, false)
-    val root: View = binding.root
-
+        _binding = FragmentHomeBinding.inflate(inflater, container, false)
+        val root: View = binding.root
+/*
     val textView: TextView = binding.textHome
     homeViewModel.text.observe(viewLifecycleOwner, Observer {
       textView.text = it
-    })
-    return root
+    })*/
+
+    bottomSheetBehavior = BottomSheetBehavior.from(binding.root.findViewById(R.id.persistent_bottom_sheet))
+
+    (activity as? AppCompatActivity)?.supportActionBar?.title = "Launches"
+
+            return root
+    }
+
+  private fun expandCollapseSheet() {
+    if (bottomSheetBehavior.state != BottomSheetBehavior.STATE_EXPANDED) {
+      bottomSheetBehavior.state = BottomSheetBehavior.STATE_EXPANDED
+    } else {
+      bottomSheetBehavior.state = BottomSheetBehavior.STATE_COLLAPSED
+    }
   }
 
-override fun onDestroyView() {
+    override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
     }
+
 }
--- app/src/main/java/com/example/spaceapp/ui/quiz/QuizFragment.kt
+++ app/src/main/java/com/example/spaceapp/ui/quiz/QuizFragment.kt
@@ -1,10 +1,13 @@
 package com.example.spaceapp.ui.quiz
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
@@ -18,7 +21,10 @@ private var _binding: FragmentQuizBinding? = null
   // This property is only valid between onCreateView and
   // onDestroyView.
   private val binding get() = _binding!!
-
+  private var shortAnimationDuration: Int = 0
+//check
+ // private val buttonStartQuiz = binding.buttonStartQuiz
+//end
   override fun onCreateView(
     inflater: LayoutInflater,
     container: ViewGroup?,
@@ -30,15 +36,41 @@ private var _binding: FragmentQuizBinding? = null
     _binding = FragmentQuizBinding.inflate(inflater, container, false)
     val root: View = binding.root
 
-    val textView: TextView = binding.textQuiz
-    notificationsViewModel.text.observe(viewLifecycleOwner, Observer {
-      textView.text = it
-    })
+    val startButton: View = root.findViewById(R.id.start)
+    val quizCard : View = root.findViewById(R.id.include)
+    val nextQuestion : View = root.findViewById(R.id.next_question)
+    val quizQuestion : TextView = root.findViewById(R.id.quiz_question)
+    var cpt : Int = 1
+    quizQuestion.text="This is the question number " + cpt.toString()
+    quizCard.visibility=View.GONE
+    nextQuestion.visibility=View.GONE
+    shortAnimationDuration = resources.getInteger(android.R.integer.config_shortAnimTime)
+
+    startButton.setOnClickListener(){
+      quizCard.visibility=View.VISIBLE
+      nextQuestion.visibility=View.VISIBLE
+      startButton.visibility=View.GONE
+    }
+
+    nextQuestion.setOnClickListener(){
+      cpt += 1
+      if (cpt>10) {
+        cpt = 1
+        quizQuestion.text = "This is the question number " + cpt.toString()
+        startButton.visibility = View.VISIBLE
+        quizCard.visibility = View.GONE
+        nextQuestion.visibility=View.GONE
+      }
+      else
+        quizQuestion.text ="This is the question number " + cpt.toString()
+
+    }
+  (activity as? AppCompatActivity)?.supportActionBar?.title = "Quiz"
     return root
   }
 
 override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
-    }
+}
 }
