ebb07b95fa92c24c47dce1ca61f0b72684ee5cab
LeiRabe <leiya.rabe@gmail.com>
2022-02-04 15:55:27 +0100

    Add debug log

--- app/src/main/java/com/example/spaceapp/ui/home/HomeFragment.kt
+++ app/src/main/java/com/example/spaceapp/ui/home/HomeFragment.kt
@@ -1,6 +1,7 @@
 package com.example.spaceapp.ui.home
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -44,6 +45,7 @@ class HomeFragment : Fragment() {
     _binding = FragmentHomeBinding.inflate(inflater, container, false)
 
     //test api
+    Log.d("FRAGMENT","test")
     api.displayAllLaunches()
 
     return binding.root
